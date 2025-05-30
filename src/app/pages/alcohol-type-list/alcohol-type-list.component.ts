27f48e08f3787c7d4b84ac7680c662012029509b
miriamCR <miriamcabana96@gmail.com>
2025-05-30 05:10:03 +0200

    Modify AlcoholTypeList

--- src/app/pages/alcohol-type-list/alcohol-type-list.component.ts
+++ src/app/pages/alcohol-type-list/alcohol-type-list.component.ts
@@ -5,29 +5,47 @@ import { RouterLink } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { CocktailService } from '../../services/cocktail.service';
 import { Cocktail } from '../home/home.component';
+import { slugify } from '../../utils/utils';
 
 @Component({
   selector: 'app-alcohol-type-list',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatCardModule, MatButtonModule],
+  imports: [CommonModule, RouterLink, MatCardModule, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './alcohol-type-list.component.html',
-  styleUrl: './alcohol-type-list.component.scss'
+  styleUrls: ['./alcohol-type-list.component.scss']
 })
 export class AlcoholTypeListComponent implements OnInit {
   cocktails: Cocktail[] = [];
   alcoholicType: string = '';
+  loading: boolean = true;
 
   constructor(private route: ActivatedRoute, private cocktailService: CocktailService) {}
 
   ngOnInit(): void {
     this.alcoholicType = this.route.snapshot.paramMap.get('type') || '';
     if (this.alcoholicType) {
-      this.cocktailService.getCocktailsByAlcoholicType(this.alcoholicType).subscribe(res => {
-        this.cocktails = res.drinks || [];
+      this.loading = true;
+      this.cocktailService.getCocktailsByAlcoholicType(this.alcoholicType).subscribe({
+        next: (res) => {
+          this.cocktails = res.drinks || [];
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('Error al cargar cócteles:', error);
+          this.cocktails = [];
+          this.loading = false;
+        }
       });
+    } else {
+      this.loading = false;
     }
+  }
+
+  getSlug(name: string): string {
+    return slugify(name);
   }
 }
